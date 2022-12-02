7a3aba030efcfc0b9bc0ab15f3de57f2128fa7ca
Ramon Brand <ramon2009@gmail.com>
2022-12-02 14:01:16 +0200

    Update example

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,9 @@ fn main() {
     let mut events = EventSystem::new();
     events.register_key_down(key_down);
 
+    events.fire_application_load();
     events.fire_key_down(EventKeyDown { key: 55 });
+    events.fire_application_exit();
 }
 
 fn key_down(packet: EventKeyDown) -> bool {
